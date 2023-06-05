42215a63d5b4264be03ee6747777cbb894d516f0
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-06-05 15:56:54 +0100

    Add confirmable to devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_140827) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_145348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_140827) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
