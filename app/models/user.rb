3d94844ef885d0c318a91b32336fbe45b440cf7e
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-06-05 16:25:04 +0100

    Add validation to user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+         validates :name, presence: true
+         has_many :categories, dependent: :destroy
+         has_many ;deals, dependent: :destroy
 end
