846593a06974748a9e68de04b45e7698b4ce7aa1
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-06-08 11:37:50 +0100

    Add request test for user

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :feature do
+  before(:each) do
+    @user = User.create(name: 'Rita Nkem', email: 'rita@gmail.com', password: '123456',
+                        password_confirmation: '123456', confirmation_token: nil, confirmed_at: Time.now)
+  end
+
+  describe 'sign in page' do
+    scenario 'should have a sign in page' do
+      visit new_user_session_path
+      expect(page).to have_content('LOGIN')
+    end
+  end
+
+  describe 'sign up page' do
+    scenario 'should have a sign up page' do
+      visit new_user_registration_path
+      expect(page).to have_content('Sign up')
+    end
+
+    scenario 'should have a sign up button' do
+      visit new_user_registration_path
+      expect(page).to have_button('Sign up')
+    end
+  end
+
+  describe 'forgot password page' do
+    it 'should have a forgot password page' do
+      visit new_user_password_path
+      expect(page).to have_content('Forgot your password?')
+    end
+
+    it 'should have a send me reset password instructions button' do
+      visit new_user_password_path
+      expect(page).to have_button('Send me reset password instructions')
+    end
+  end
+end
