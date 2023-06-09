a20ce77bad6a021a77209afbc873160b5bbe2ac8
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-06-09 10:06:15 +0100

    Add test

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'Users', type: :feature do
   describe 'sign up page' do
     scenario 'should have a sign up page' do
       visit new_user_registration_path
-      expect(page).to have_content('Sign up')
+      expect(page).to have_content('REGISTRATION')
     end
 
     scenario 'should have a sign up button' do
