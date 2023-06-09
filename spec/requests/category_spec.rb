451050ddf18eceedca26fed2af2dff014a0bae60
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-06-09 14:57:39 +0100

    Refractor test

--- spec/requests/category_spec.rb
+++ spec/requests/category_spec.rb
@@ -1,26 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature 'CategoriesIndices', type: :feature do
+  let(:file) { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'test1.webp'), 'image/webp') }
+
+  before :each do
+    visit new_user_session_path
+    @user = User.create(name: 'ezekiel', email: 'ezekiel@example.com', password: '1111111111')
+    @category = Category.create(name: 'Food', author: @user)
+    @category.icon.attach(file)
+    @categorytwo = Category.create(name: 'Football', author: @user)
+    @categorytwo.icon.attach(file)
+    @categorythree = Category.create(name: 'Tacos', author: @user)
+    @categorythree.icon.attach(file)
+    @transaction = Deal.create(name: 'Athletics', amount: 100, author: @user)
+
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_on 'Log in'
+  end
+
   describe 'Categories Index' do
-    let(:file) { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'test1.webp'), 'image/webp') }
-    before :each do
-      visit new_user_session_path
-      @user = User.create(name: 'ezekiel', email: 'ezekiel@example.com', password: '1111111111')
-      @category = Category.create(name: 'Food', author: @user)
-      @category.icon.attach(file)
-      @categorytwo = Category.create(name: 'Football', author: @user)
-      @categorytwo.icon.attach(file)
-      @categorythree = Category.create(name: 'Tacos', author: @user)
-      @categorythree.icon.attach(file)
-      @transaction = Deal.create(name: 'Athletics', amount: 100, author: @user)
-      
-      puts @category.errors.full_messages
-
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      click_on 'Log in'
-    end
-
     it 'should have a title' do
       expect(page).to have_current_path(categories_path)
       expect(page).to have_content('Categories')
@@ -30,9 +29,12 @@ RSpec.feature 'CategoriesIndices', type: :feature do
       expect(page).to have_current_path(categories_path)
       expect(page).to have_content('Add New category')
     end
+  end
 
-    it 'should have a category name' do
-      expect(page).to have_current_path(categories_path)
+  describe 'Category names' do
+    before { visit categories_path }
+
+    it 'should display category names' do
       @categories = Category.all
 
       @categories.each do |category|
@@ -43,7 +45,6 @@ RSpec.feature 'CategoriesIndices', type: :feature do
     end
 
     it 'should link to the transaction index' do
-    expect(page).to have_current_path(categories_path)
       click_on @category.name
       expect(page).to have_current_path(category_deals_path(@category.id))
       expect(page).to have_content('TRANSACTION')
